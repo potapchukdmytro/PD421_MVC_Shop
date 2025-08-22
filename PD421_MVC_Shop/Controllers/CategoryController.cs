50dd98a034a76fa84fba56416deee51c5dc15ce1
Dmytro Potapchuk <traiget@gmail.com>
2025-08-22 17:47:00 +0300

    CRUD operations for Categories

--- PD421_MVC_Shop/Controllers/CategoryController.cs
+++ PD421_MVC_Shop/Controllers/CategoryController.cs
@@ -18,5 +18,69 @@ namespace PD421_MVC_Shop.Controllers
 
             return View(categories);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Для захисту від CSRF атак
+        public IActionResult Create(Category model)
+        {
+            bool res = _context.Categories.Any(c => c.Name.ToLower() == model.Name.ToLower());
+            if(res)
+            {
+                return View();
+            }
+
+            _context.Categories.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET
+        public IActionResult Update(int id)
+        {
+            var model = _context.Categories.Find(id);
+            if(model == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Category model)
+        {
+            bool res = _context.Categories.Any(c => c.Name.ToLower() == model.Name.ToLower());
+            if (res)
+            {
+                return View();
+            }
+
+            _context.Update(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET
+        public IActionResult Delete(int id)
+        {
+            var model = _context.Categories.Find(id);
+            if (model == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _context.Categories.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- PD421_MVC_Shop/wwwroot/js/site.js
+++ PD421_MVC_Shop/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var pageModal = null;
+
+function openModal(id) {
+    var link = document.getElementById("deleteCategoryAction");
+    link.href = "/Category/Delete/" + id;
+    pageModal = new bootstrap.Modal(document.getElementById("categoryDeleteModal"), { keyboard: false });
+    pageModal.show();
+}
+
+function closeModal() {
+    if (pageModal != null) {
+        pageModal.hide();
+        pageModal = null;
+    }
+}
